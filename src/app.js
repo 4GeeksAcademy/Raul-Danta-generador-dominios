113c870d33166d89772762c15d4278b381531e02
Raul Danta Mosquera <rdanta09@gmail.com>
2025-10-17 18:12:17 +0000

    corrección

--- src/app.js
+++ src/app.js
@@ -5,18 +5,13 @@ window.onload = function() {
     let dom = ['.com', '.es', '.org'];
 
     for (let i = 0; i < pronoun.length; i++) {
-    
-      for (let j = 0; j < adj.length; j++) {
-        
+     for (let j = 0; j < adj.length; j++) {
         for (let k = 0; k < noun.length; k++) {
-          
-            for (let p = 0; p < dom.length; p++) {
+           for (let p = 0; p < dom.length; p++) {
+
               console.log (pronoun[i]+adj[j]+noun[k]+dom[p])
-              
             }
         }
-        
       }
-      
     }
   }
